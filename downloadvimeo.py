f4f088576b4c6e86fad935cdbda2eb6a4d7ef19c
Lucas Barizon <4043427+barizonlucas@users.noreply.github.com>
2025-12-01 22:10:22 -0300

    initial commit

---
+++ downloadvimeo.py
@@ -0,0 +1,13 @@
+# Salve como download.py e rode: python3 download.py
+
+import yt_dlp
+
+url = "https://player.vimeo.com/video/861371434"
+
+ydl_opts = {
+    'cookiefile': None,           # None = usa cookies do navegador padrão
+    'cookiesfrombrowser': ('chrome',),  # mude para ('safari',) se usar Chrome
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
